631cc394dd55e0311a7f1c14638977430debd798
Xurvan <xurvan@tutanota.com>
2024-06-22 20:54:40 +0400

    Rename folders

---
+++ apps/app2/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/xurvan/monorepo-template/tools/foo"
+
+func main() {
+	foo.LogFoo()
+}
--- backend/srvs/srv2/cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/xurvan/monorepo-template/backend/libs/foo"
-
-func main() {
-	foo.LogFoo()
-}
