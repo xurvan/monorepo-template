fe84d2a5f04822c5049e97d73b14a88058dc8d1e
Xurvan <xurvan@tutanota.com>
2024-07-01 17:11:53 +0400

    Improve service and server variable names

--- apps/app3/cmd/main.go
+++ apps/app3/cmd/main.go
@@ -19,11 +19,11 @@ func (s *HelloService) SayHello(ctx context.Context, req *pb.SayHelloRequest) (r
 }
 
 func main() {
-	server := &HelloService{}
-	twirpHandler := pb.NewHelloServiceServer(server)
+	svc := &HelloService{}
+	srv := pb.NewHelloServiceServer(svc)
 
 	log.Println("Serving HTTP on 0.0.0.0:8080")
-	err := http.ListenAndServe(":8080", twirpHandler)
+	err := http.ListenAndServe(":8080", srv)
 	if err != nil {
 		panic(err)
 	}
